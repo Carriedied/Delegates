c45e80cfe3afe53c9389b8519f7f1201b8eaf4bd
Carriedied <52948394+Carriedied@users.noreply.github.com>
2020-05-12 15:13:21 +0700

    Add files via upload

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Unsafe
+    {
+            
+        public delegate void Mydelegate(double a);
+
+        unsafe internal void mas(double a, int b)
+        {
+            Mydelegate[] md = new Mydelegate[]
+            {
+                division, multiplication, addition, subtraction
+            };
+
+            switch (b)
+            {
+                case 1:
+                    md[b - 1](Convert.ToDouble(a));
+                    break;
+                case 2:
+                    md[b - 1](Convert.ToDouble(a));
+                    break;
+                case 3:
+                    md[b - 1](Convert.ToDouble(a));
+                    break;
+                case 4:
+                    md[b - 1](Convert.ToDouble(a));
+                    break;
+                default:
+                    Program.input1();
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void division(double number_one)
+        {
+            double c = number_one / number_one;
+            Console.WriteLine("division: " + c + " ");
+        }
+
+        static void multiplication(double number_one)
+        {
+            double c = number_one * number_one;
+            Console.WriteLine("multiplication: " + c + " ");
+        }
+
+        static void addition(double number_one)
+        {
+            double c = number_one + number_one;
+            Console.WriteLine("addition: " + c + " ");
+        }
+
+        static void subtraction(double number_one)
+        {
+            double c = number_one - number_one;
+            Console.WriteLine("subtraction: " + c + " ");
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!\n");
+
+            input1();
+        }
+
+        public static void input(int c)
+        {
+            Unsafe uns = new Unsafe();
+
+            double d;
+            int e;
+            switch (c)
+            {
+                case 1:
+                    Environment.Exit(0);
+                    break;
+                case 2:
+                    Console.Write("Введите число, благодаря которому будут производиться вычисления = ");
+                    while (!double.TryParse(Console.ReadLine(), out d))
+                    {
+                        Console.WriteLine("Ошибка ввода!\n");
+                        input(c);
+                    }
+                    Console.Write("Введите действие, которое хотите выполнить:\n" +
+                        "1.Деление само на себя\n" +
+                        "2.Умножение само на себя\n" +
+                        "3.Сложение самого себя\n" +
+                        "4.Вычитание из самого себя\n");
+                    while (!int.TryParse(Console.ReadLine(), out e))
+                    {
+                        Console.WriteLine("Ошибка ввода!\n");
+                        input(c);
+                    }
+                    uns.mas(d, e);
+                    input1();
+                    break;
+                default:
+                    Console.Write("Введите значение снова.\n");
+                    input1();
+                    break;
+            } 
+        }
+        public static void input1()
+        {
+            int c;
+            Console.Write("Выберите дальнейшее действие:\n" +
+                "1.Закончить рабту\n" +
+                "2.Продолжить\n");
+            while (!int.TryParse(Console.ReadLine(), out c))
+            {
+                Console.WriteLine("Ошибка ввода!\n");
+                input(c);
+            }
+            input(c);
+        }
+    }
+}
